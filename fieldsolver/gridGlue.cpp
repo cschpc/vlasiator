1af46bae91339b9180bad785fb432e8f048c73a2
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-10-16 14:06:36 +0300

    Fix function calls; Update fsgrid

--- fieldsolver/gridGlue.cpp
+++ fieldsolver/gridGlue.cpp
@@ -64,17 +64,16 @@ template <typename T, int stencil> void computeCoupling(dccrg::Dccrg<SpatialCell
   for (FsGridTools::FsIndex_t k=0; k<gridDims[2]; k++) {
     for (FsGridTools::FsIndex_t j=0; j<gridDims[1]; j++) {
       for (FsGridTools::FsIndex_t i=0; i<gridDims[0]; i++) {
-        const std::array<FsGridTools::FsIndex_t, 3> globalIndices = momentsGrid.getGlobalIndices(i,j,k);
-        const dccrg::Types<3>::indices_t  indices = {{(uint64_t)globalIndices[0],
-                        (uint64_t)globalIndices[1],
-                        (uint64_t)globalIndices[2]}}; //cast to avoid warnings
-        CellID dccrgCell = mpiGrid.get_existing_cell(indices, 0, mpiGrid.mapping.get_maximum_refinement_level());
-        
-        int process = mpiGrid.get_process(dccrgCell);
-        FsGridTools::LocalID fsgridLid = momentsGrid.LocalIDForCoords(i,j,k);
-        //int64_t  fsgridGid = momentsGrid.GlobalIDForCoords(i,j,k);
-        onFsgridMapRemoteProcess[process].insert(dccrgCell); //cells are ordered (sorted) in set
-        onFsgridMapCells[dccrgCell].push_back(fsgridLid);
+         const std::array<FsGridTools::FsIndex_t, 3> globalIndices = momentsGrid.localToGlobal(i, j, k);
+         const dccrg::Types<3>::indices_t indices = {{(uint64_t)globalIndices[0], (uint64_t)globalIndices[1],
+                                                      (uint64_t)globalIndices[2]}}; // cast to avoid warnings
+         CellID dccrgCell = mpiGrid.get_existing_cell(indices, 0, mpiGrid.mapping.get_maximum_refinement_level());
+
+         int process = mpiGrid.get_process(dccrgCell);
+         FsGridTools::LocalID fsgridLid = momentsGrid.LocalIDForCoords(i, j, k);
+         // int64_t  fsgridGid = momentsGrid.GlobalIDForCoords(i,j,k);
+         onFsgridMapRemoteProcess[process].insert(dccrgCell); // cells are ordered (sorted) in set
+         onFsgridMapCells[dccrgCell].push_back(fsgridLid);
       }
     }
   }
