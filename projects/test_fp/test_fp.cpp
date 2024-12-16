a1615441840a5db80974183c9da1c956297872d8
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-12-16 14:50:21 +0200

    Replace fsgrid with span

--- projects/test_fp/test_fp.cpp
+++ projects/test_fp/test_fp.cpp
@@ -110,8 +110,8 @@ namespace projects {
       fsgrid::FsGrid< std::array<Real, fsgrids::bgbfield::N_BGB>, FS_STENCIL_WIDTH> & BgBGrid,
       fsgrid::FsGrid< fsgrids::technical, FS_STENCIL_WIDTH> & technicalGrid
    ) {
-      setBackgroundFieldToZero(BgBGrid);
-      
+      setBackgroundFieldToZero(BgBGrid.getData());
+
       if(!P::isRestart) {
          auto localSize = perBGrid.getLocalSize().data();
          const auto& gridSpacing = perBGrid.getGridSpacing();
