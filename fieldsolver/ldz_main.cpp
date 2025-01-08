16489017ca00dfd7907a81ac816ca821bd52506b
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-01-08 10:27:30 +0200

    Change updateGhostCells calls to use technicalGrid with a span of data

--- fieldsolver/ldz_main.cpp
+++ fieldsolver/ldz_main.cpp
@@ -93,6 +93,7 @@ bool propagateFields(fsgrid::FsGrid<std::array<Real, fsgrids::bfield::N_BFIELD>,
 
    const auto* localSize = &technicalGrid.getLocalSize()[0];
    std::span<fsgrids::technical> technical = technicalGrid.getData();
+   std::span<const std::array<Real, fsgrids::volfields::N_VOL>> vol = volGrid.getData();
 
 #pragma omp parallel for collapse(2)
    for (auto k = 0; k < localSize[2]; k++) {
@@ -293,7 +294,7 @@ bool propagateFields(fsgrid::FsGrid<std::array<Real, fsgrids::bfield::N_BFIELD>,
    calculateVolumeAveragedFieldsSimple(perBGrid, EGrid, dPerBGrid, volGrid, technicalGrid);
    calculateBVOLDerivativesSimple(volGrid, technicalGrid);
    if (FieldTracing::fieldTracingParameters.doTraceFullBox || Parameters::computeCurvature) {
-      volGrid.updateGhostCells();
+      technicalGrid.updateGhostCells(vol);
       calculateCurvatureSimple(volGrid, BgBGrid, technicalGrid);
    }
    return true;
